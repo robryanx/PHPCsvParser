4c8c3f69a91e1adecf7aa45e104410139dc26234
Rob Ryan <rob@affclicks.com>
2018-08-27 12:18:24 +1000

    Handle the case where the field is an empty string between enclosures

--- src/KzykHys/CsvParser/Iterator/CsvIterator.php
+++ src/KzykHys/CsvParser/Iterator/CsvIterator.php
@@ -96,7 +96,7 @@ class CsvIterator implements \Iterator
                 // check the first letter is 'enclosure' or not
                 if (substr($value, 0, 1) === $this->option['enclosure']) {
                     // check the last letter is 'enclosure', but not the second last letter which would be escaping the enclosure
-                    if (substr($value, -1) === $this->option['enclosure'] && (strlen($value) === 1 || substr($value, -2, 1) !== $this->option['enclosure'])) {
+                    if (substr($value, -1) === $this->option['enclosure'] && (in_array(strlen($value), [1,2]) || substr($value, -2, 1) !== $this->option['enclosure'])) {
                         // case where the first and last letter is the same enclosure
                         if(strlen($value) === 1) {
                             $this->processClosingField($value, $this->option);
@@ -108,7 +108,7 @@ class CsvIterator implements \Iterator
                     }
                 } else { // first letter is NOT 'enclosure'
                     // check the last letter is 'enclosure', but not the second last letter which would be escaping the enclosure
-                    if(substr($value, -1) === $this->option['enclosure'] && (strlen($value) === 1 || substr($value, -2, 1) !== $this->option['enclosure'])) {
+                    if(substr($value, -1) === $this->option['enclosure'] && (in_array(strlen($value), [1,2]) || substr($value, -2, 1) !== $this->option['enclosure'])) {
                         $this->processClosingField($value, $this->option);
                     } else {
                         $this->processField($value, $this->option);
